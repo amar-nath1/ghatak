74748de05f05fdcca5b868c68f99f8fce6de8588
srikrishna <krishna199987@gmail.com>
2024-01-07 22:19:13 +0530

    integrated json data to card changes in tab 2

--- src/app/shared/services/accounts/accounts.service.ts
+++ src/app/shared/services/accounts/accounts.service.ts
@@ -1,29 +1,29 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { map } from 'rxjs';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AccountsService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http:HttpClient) {
+  fetchAllProfiles() {
+    return this.http.get('../../../../assets/customers.json').pipe(
+      map((res) => {
+        return res;
+      })
+    );
+  }
 
-
-
-   }
-
-   fetchAllProfiles(){
-    
-   }
-
-   postProfile(profile:any){
-  
-    const payload = JSON.stringify(profile)
+  postProfile(profile: any) {
+    const payload = JSON.stringify(profile);
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
-    })
-    return this.http.post('http://localhost:3000/api/users',payload,{headers}).subscribe((res)=>{
-      console.log(res,' backseres')
-    })
-   }
+    });
+    return this.http
+      .post('http://localhost:3000/api/users', payload, { headers })
+      .subscribe((res) => {
+        console.log(res, ' backseres');
+      });
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-
+import { AccountsService } from '../shared/services/accounts/accounts.service';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
-
-  constructor() {}
-
+  public userData: any = [];
+  constructor(private accountService: AccountsService) {
+    this.getUserData();
+  }
+  getUserData() {
+    this.accountService.fetchAllProfiles().subscribe((res) => {
+      this.userData = res;
+      console.log(this.userData, 'USERDATA---->');
+    });
+  }
 }
